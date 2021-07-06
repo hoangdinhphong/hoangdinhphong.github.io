99b6da5a945b3d131d3ad7444bc50cb8e76ec6af
hoangdinhphong <hoangtuananh15888@gmail.com>
2021-07-06 11:33:12 +0800

    -

--- update.sh
+++ update.sh
@@ -50,12 +50,12 @@ echo "{}]" >> all.pkgs
 }
 echo "------------------"
 echo "Building Packages...."
-apt-ftparchive packages ./Deb > ./Packages;
+apt-ftparchive packages ./Deb > ./Info/Win10Clock/Boss/Packages;
 #sed -i -e '/^SHA/d' ./Packages;
-bzip2 -c9k ./Packages > ./Packages.bz2;
+bzip2 -c9k ./Info/Win10Clock/Boss/Packages > ./Packages.bz2;
 echo "------------------"
 echo "Building Release...."
-printf "Origin: Hoàng Đình Phong\nLabel: Hoàng Đình Phong\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: Hoàng Đình Phong\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: Hoàng Đình Phong\nLabel: Hoàng Đình Phong\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: Hoàng Đình Phong\nMD5Sum:\n "$(cat ./Info/Win10Clock/Boss/Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Info/Win10Clock/Boss/Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Info/Win10Clock/Boss/Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 
 echo "------------------"
 echo "Done!"
