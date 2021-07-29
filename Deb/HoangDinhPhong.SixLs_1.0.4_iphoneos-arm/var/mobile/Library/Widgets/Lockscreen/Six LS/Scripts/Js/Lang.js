6b6b30faaaf6f8d167036018f6107b123d324667
hoangdinhphong <hoangtuananh15888@gmail.com>
2021-07-29 21:28:33 +0800

    -

--- Deb/HoangDinhPhong.SixLs_1.0.4_iphoneos-arm/var/mobile/Library/Widgets/Lockscreen/Six LS/Scripts/Js/Lang.js
+++
@@ -1,9 +0,0 @@
-if (window.config.language == "Vietnamese") {
-var days = ["Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"];
-var months = ["tháng 1", "tháng 2", "tháng 3", "tháng 4", "tháng 5", "tháng 6", "tháng 7", "tháng 8", "tháng 9", "tháng 10", "tháng 11", "tháng 12"];
-var titletext = ["♫ Music ♫"];
-} else {
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var titletext = ["♫ Music ♫"];
-}
--- Deb/HoangDinhPhong.SixLs_1.0.4_iphoneos-arm/var/mobile/Library/Widgets/Lockscreen/Six LS/Scripts/Js/Weather.js
+++
@@ -1,9 +0,0 @@
-function mainUpdate(type){ 
-if (type === "weather"){checkWeather();
-}
-if (type === "music"){checkMusic();
-}
-
-function checkWeather(){
-document.getElementById("City").innerHTML = weather.city;}
-}
