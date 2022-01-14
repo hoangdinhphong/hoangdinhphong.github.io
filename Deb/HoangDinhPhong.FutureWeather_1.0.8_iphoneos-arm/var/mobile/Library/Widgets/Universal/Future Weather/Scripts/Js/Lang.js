8cc3cd17b26e125f579ab3f80e955bf9eb3c8319
System Administrator <hoangtuananh15888@gmail.com>
2022-01-14 21:10:55 +0800

    -

--- Deb/HoangDinhPhong.FutureWeather_1.0.8_iphoneos-arm/var/mobile/Library/Widgets/Universal/Future Weather/Scripts/Js/Lang.js
+++
@@ -1,9 +0,0 @@
-if (window.config.language == "Vietnamese") {
-var charging = ["Đang sạc pin..."];
-var notcharging = ["Lượng pin còn lại "];
-var uptext = ["Cập nhật lúc: "];
-} else {
-var charging = ["Charging..."];
-var notcharging = ["Battery level at "];
-var uptext = ["Lats Update: "];
-}
