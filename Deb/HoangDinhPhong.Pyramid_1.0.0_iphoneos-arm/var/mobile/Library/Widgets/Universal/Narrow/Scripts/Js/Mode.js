42242f6ec7c2207ee44a1ae3e407e094cabbfe76
System Administrator <hoangtuananh15888@gmail.com>
2022-03-22 15:09:21 +0800

    -

--- Deb/HoangDinhPhong.Pyramid_1.0.0_iphoneos-arm/var/mobile/Library/Widgets/Universal/Narrow/Scripts/Js/Mode.js
+++
@@ -1,8 +0,0 @@
-$('head').removeAttr('Mode');
-if (config.Mode == 'Light') {
-$ ('head').append('<link rel="stylesheet" media="screen" href="Scripts/Css/Light.css" type="text/css" >');
-}
-
-else if (config.Mode == 'Dark') {
-$ ('head').append('<link rel="stylesheet" media="screen" href="Scripts/Css/Dark.css" type="text/css" >');
-}
--- Deb/HoangDinhPhong.Pyramid_1.0.0_iphoneos-arm/var/mobile/Library/Widgets/Universal/Narrow/Scripts/Js/Settings.js
+++
@@ -1,6 +0,0 @@
-document.documentElement.style.setProperty('--textStyle', config.textStyle);
-document.body.style.webkitTransform = 'scale(' + config.Scale + ')';
-/*On off*/
-if(!config.Al){
-document.getElementById('AmLich').style.display = 'none';
-}
